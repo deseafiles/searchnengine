a9ca613a262decaf8e229f6a07bfb35eea87fb42
deseafiles <shabrina.bhicci@gmail.com>
2024-12-01 18:45:45 +0800

    tes

--- app/src/main/java/search/Utils/PrintJson.java
+++ app/src/main/java/search/Utils/PrintJson.java
@@ -62,44 +62,5 @@ public static List<String> printRedBlackTreeResults(RbTree rbTree, String search
     }
 
     return combinedResults;
-
-        // if (resultByKey != null && !resultByKey.isEmpty()) {
-        //     for (Node node : resultByKey) {
-        //         String key = node.getKey().toString();
-        //         String value = node.getValue() != null ? node.getValue().toString() : "null";
-                
-        //         String resultString = "> " + key + "\n " + value;
-
-        //         if (!combinedResults.contains(resultString)) {
-        //             combinedResults.add(resultString); 
-        //         }
-        //     }
-        // }
-
-    // public static List<String> printRedBlackTreeResults(RbTree rbTree, String searchParam) {
-    //     List<Node> result = rbTree.search(searchParam);  
-    //     List<Node> resultByKey = rbTree.searchByKey(searchParam);
-    //     ArrayList<String> combinedResults = new ArrayList<>();
-    //     GameGimmick gimmick = new GameGimmick();
-
-    //     if (result != null && !result.isEmpty()) {
-    //         for (Node node : result) {
-    //             String key = node.getKey().toString(); 
-    //             String value;
-
-    //             if (node.gimmick != null && key.equals(searchParam)) {
-    //                 value = gimmick.gimmick(key);
-    //             } else {
-    //                 value = node.getValue() != null ? node.getValue().toString() : "null"; 
-    //             }
-
-    //             String resultString = "> " + key + "\n " + value;
-
-    //             if (!combinedResults.contains(resultString)) {
-    //                 combinedResults.add(resultString); 
-    //             }
-    //         }
-    //     }
-    //     return combinedResults; 
     }
 }
